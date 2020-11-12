f66dbc06d89c35ad7c5458e719dae7fa7e1a9599
beishui-131 <luchangtiangao@gmail.com>
2020-11-12 11:44:24 +0800

    停

--- hospitalpro/src/main/java/com/example/hospitalpro/user/mapper/UserMapper.java
+++ hospitalpro/src/main/java/com/example/hospitalpro/user/mapper/UserMapper.java
@@ -1,7 +1,15 @@
 package com.example.hospitalpro.user.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.hospitalpro.common.entity.Regi;
 
 @Mapper
 public interface UserMapper {
+	// 挂号
+	@Insert("insert into regi(d_id,pa_id,time)")
+	List<Regi> registr();
 }
